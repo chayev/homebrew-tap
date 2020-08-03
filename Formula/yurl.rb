da14e2cac02b57b458ad7aaaec93bb8f26f5fe9e
Michael Ibragimchayev <michael.ibragimchayev@gmail.com>
2020-08-03 13:31:34 -0400

    Release: v0.1.1

--- Formula/yurl.rb
+++ Formula/yurl.rb
@@ -1,12 +1,12 @@
 class Yurl < Formula
   desc "AASA / Universal Links Validator."
   homepage "https://github.com/chayev/yurl"
-  version "0.1.0"
+  version "0.1.1"
   bottle :unneeded
   license "MIT"
 
-  url "https://github.com/chayev/yurl/releases/download/v0.1.0/yurl-v0.1.0-macOS-amd64.tar.gz"
-  sha256 "6855122671874827969ae1a3b4cc411dbe654de82f1f0a742339da5f72f0ede9"
+  url "https://github.com/chayev/yurl/releases/download/v0.1.1/yurl-v0.1.1-macOS-amd64.tar.gz"
+  sha256 "4cee766650d72aae8d7afcee100f79671ba8fe57bfa684b66f0423643576d912"
 
   
   def install
