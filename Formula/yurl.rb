d1f4fa9ac4bb6f8d54e89e64faaa37ee3e785a59
Michael Ibragimchayev (chayev) <michael@chayev.com>
2020-09-03 22:22:59 -0400

    Brew formula update for yurl version v0.3.2

--- Formula/yurl.rb
+++ Formula/yurl.rb
@@ -2,16 +2,16 @@
 class Yurl < Formula
   desc "Universal Links / AASA File Validator."
   homepage "https://github.com/chayev/yurl"
-  version "0.3.1"
+  version "0.3.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/chayev/yurl/releases/download/v0.3.1/yurl-v0.3.1-macos-amd64.tar.gz"
-    sha256 "60431b2355f39811dc701993c3224275360cf3fc7b73c7eb600b5059a4b98a85"
+    url "https://github.com/chayev/yurl/releases/download/v0.3.2/yurl-v0.3.2-macos-amd64.tar.gz"
+    sha256 "bafd3d6bc258a36d95f85221d4293912f1f109459f75a9efc8ca2638515329a2"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/chayev/yurl/releases/download/v0.3.1/yurl-v0.3.1-linux-amd64.tar.gz"
-      sha256 "ae508c3ae36532f8d73010b60e627957a20814c77edc0f896e08f0615713f050"
+      url "https://github.com/chayev/yurl/releases/download/v0.3.2/yurl-v0.3.2-linux-amd64.tar.gz"
+      sha256 "aef7fbacf0617defe903c983306e2eb70059e636d437eb93e2e25a9ab810f711"
     end
   end
 
