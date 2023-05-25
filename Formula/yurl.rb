992c2a552edaacb88a7677bd0de21c4a1589af07
Michael Ibragimchayev (chayev) <michael@chayev.com>
2023-05-25 11:50:41 -0400

    Brew formula update for yurl version v0.8.4

--- Formula/yurl.rb
+++ Formula/yurl.rb
@@ -5,11 +5,11 @@
 class Yurl < Formula
   desc "Universal Links / AASA File Validator."
   homepage "https://github.com/chayev/yurl"
-  version "0.8.3"
+  version "0.8.4"
 
   on_macos do
-    url "https://github.com/chayev/yurl/releases/download/v0.8.3/yurl-v0.8.3-macos-amd64.tar.gz"
-    sha256 "6b58f3451a15240f221816e6fe5461b6704a42ccdc4bae4017670887ed780a16"
+    url "https://github.com/chayev/yurl/releases/download/v0.8.4/yurl-v0.8.4-macos-amd64.tar.gz"
+    sha256 "07932e0da0b8a45abc018b65f11afb3304bf6aa27013cca7aef242d531a5d87e"
 
     def install
       bin.install "yurl"
@@ -28,8 +28,8 @@ class Yurl < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/chayev/yurl/releases/download/v0.8.3/yurl-v0.8.3-linux-amd64.tar.gz"
-      sha256 "5677089efd2134d7a6880aeacccab0e8ad70a611d3c763f0bcafd5c6ece19aa7"
+      url "https://github.com/chayev/yurl/releases/download/v0.8.4/yurl-v0.8.4-linux-amd64.tar.gz"
+      sha256 "3c5520baf5e33c6c6f7a843f7c6794d498158b20eb3941e59e3cb693c82fe57b"
 
       def install
         bin.install "yurl"
